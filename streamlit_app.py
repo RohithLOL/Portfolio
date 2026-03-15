d30026e9f40e2f8bf1447a195dbdb1817c2e67a4
RohithLOL <rsarohith@gmail.com>
2026-03-15 04:50:51 +0000

    tester 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,30 @@
+import requests
 import streamlit as st
+import time
+from streamlit_js_eval import streamlit_js_eval
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+image_path = "https://cdn.esawebb.org/archives/images/screen/potm2410a.jpg"
+
+st.markdown(
+    f"""
+    <body>
+    <img src={image_path}>
+    </body>
+    """,
+    unsafe_allow_html=True
 )
+
+
+coolPic = st.container()
+
+if st.button("Home"):
+    st.switch_page("streamlit_app.py")
+if st.button("Page 1"):
+    st.switch_page("pages/one.py")
+
+with coolPic: 
+    with st.spinner("Loading files"):
+        url = "https://drive.google.com/uc?export=view&id=1UX1ORnpUj87_3CmLZu9C4KSs1WdFJi_C"
+        response = requests.get(url)
+        coolPic = st.image(response.content)
+        time.sleep(0.5)
